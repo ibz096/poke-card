2f510b848b36086f0577ee3f2e5feeb2dec1f270
ibz096 <ibrahim.ahmed1996@gmail.com>
2022-01-29 23:24:04 -0500

    Added data pull from PokeAPI

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 document.getElementById('generate').addEventListener("click", generate);
 
 function generate() {
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(function(data) {
+        console.log(data);
+        document.getElementById('name').innerHTML = data.name;
+        document.getElementById('hp').innerHTML = data.stats[0].base_stat;
+        document.getElementById('attack').innerHTML = data.stats[1].base_stat;
+        document.getElementById('defense').innerHTML = data.stats[2].base_stat;
+        document.getElementById('speed').innerHTML = data.stats[5].base_stat;
+    });
 }
