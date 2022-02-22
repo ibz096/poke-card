6615cf8b33474edf031b028ae608ec6fc8186a63
ibz096 <ibrahim.ahmed1996@gmail.com>
2022-02-21 20:29:02 -0500

    Defined an object of types to be used later

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
 const url = 'https://pokeapi.co/api/v2/pokemon';
 
+const type_map = {
+    normal: "rgb(168 168 120)",
+    fire: "rgb(240 128 48)",
+    water: "rgb(104 144 240)",
+    grass: "rgb(120 200 80)",
+    electric: "rgb(248 208 48)",
+    ice: "rgb(152 216 216)",
+    fighting: "rgb(192 48 40)",
+    poison: "rgb(160 64 160)",
+    ground: "rgb(224 192 104)",
+    flying: "rgb(168 144 240)",
+    psychic: "rgb(248 88 136)",
+    bug: "rgb(168 144 240)",
+    rock: "rgb(184 160 56)",
+    ghost: "rgb(112 88 152)",
+    dark: "rgb(112 88 72)",
+    dragon: "rgb(112 56 248)",
+    steel: "rgb(184 184 208)",
+    fairy: "rgb(240 182 188)"
+}
+
 document.getElementById('generate').addEventListener("click", generate);
 
 function capitalizeFirstLetter(string) {
